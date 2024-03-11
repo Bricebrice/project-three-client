b616db52ecc7f71ea764b47307cfe795bf2842db
Ricardo Vazquez <rickyrabv@gmail.com>
2024-03-11 02:06:02 +0100

    Styling Login and Signup Pages

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -4,21 +4,24 @@ import axios from "axios";
 
 const API_URL = "http://localhost:5005";
 
-function SignupPage(props) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [passVerify, setPassVerify] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
+export default function SignupPage() {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+    passVerify: "",
+    firstName: "",
+    lastName: "",
+  });
   const [errorMessage, setErrorMessage] = useState(undefined);
 
   const navigate = useNavigate();
 
-  const handleEmail = (e) => setEmail(e.target.value);
-  const handlePassword = (e) => setPassword(e.target.value);
-  const handlePassVerify = (e) => setPassVerify(e.target.value);
-  const handleFirstName = (e) => setFirstName(e.target.value);
-  const handleLastName = (e) => setLastName(e.target.value);
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.id]: e.target.value,
+    });
+  };
 
   const handleSignupSubmit = async (e) => {
     e.preventDefault();
@@ -29,70 +32,122 @@ function SignupPage(props) {
     //   return;
     // }
 
-    const requestBody = { firstName, lastName, email, password, passVerify };
+    const requestBody = { form };
 
     try {
       const response = await axios.post(`${API_URL}/auth/signup`, requestBody);
       // console.log("Signup response:", response.data);
-
+      setForm({
+        email: "",
+        password: "",
+        passVerify: "",
+        firstName: "",
+        lastName: "",
+      });
       navigate("/login");
     } catch (error) {
       if (error.response) {
         const errorDescription = error.response.data.message;
         setErrorMessage(errorDescription);
+        return;
       }
     }
   };
 
   return (
-    <div className="SignupPage">
-      <h1>Sign Up</h1>
+    <div className="SignupPage bg-yellow-300 flex flex-col h-dvh items-center">
+      <h1 className="text-3xl p-4">Sign Up</h1>
 
-      <form onSubmit={handleSignupSubmit}>
-        <label>First name:</label>
-        <input
-          type="text"
-          name="firstName"
-          value={firstName}
-          onChange={handleFirstName}
-        />
+      <form
+        onSubmit={handleSignupSubmit}
+        className="max-w-sm mx-auto flex flex-col items-center mb-5 bg-yellow-400 rounded p-7"
+      >
+        <div className="mb-5 flex gap-3">
+          <div>
+            <label
+              htmlFor="firstName"
+              className="block mb-2 text-sm font-medium"
+            >
+              First name:
+            </label>
+            <input
+              className="bg-gray-100 border border-gray-400 text-sm rounded-lg w-full p-2.5 "
+              type="text"
+              name="firstName"
+              id="firstName"
+              value={form.firstName}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="lastName"
+              className="block mb-2 text-sm font-medium"
+            >
+              Last name:
+            </label>
+            <input
+              className="bg-gray-100 border border-gray-400 text-sm rounded-lg w-full p-2.5 "
+              id="lastName"
+              type="text"
+              name="lastName"
+              value={form.lastName}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+        <div className="w-full mb-5">
+          <label htmlFor="email" className="block mb-2 text-sm font-medium">
+            Email:
+          </label>
+          <input
+            className="bg-gray-100 border border-gray-400 text-sm rounded-lg w-full p-2.5 "
+            id="email"
+            type="email"
+            name="email"
+            value={form.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-5 flex gap-3">
+          <div>
+            <label className="block mb-2 text-sm font-medium">Password:</label>
+            <input
+              className="bg-gray-100 border border-gray-400 text-sm rounded-lg w-full p-2.5 "
+              type="password"
+              name="password"
+              value={form.password}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label className="block mb-2 text-sm font-medium">
+              Verify password:
+            </label>
+            <input
+              className="bg-gray-100 border border-gray-400 text-sm rounded-lg w-full p-2.5 "
+              type="password"
+              name="passVerify"
+              value={form.passVerify}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
 
-        <label>Last name:</label>
-        <input
-          type="text"
-          name="lastName"
-          value={lastName}
-          onChange={handleLastName}
-        />
-
-        <label>Email:</label>
-        <input type="email" name="email" value={email} onChange={handleEmail} />
-
-        <label>Password:</label>
-        <input
-          type="password"
-          name="password"
-          value={password}
-          onChange={handlePassword}
-        />
-
-        <label>Verify password:</label>
-        <input
-          type="password"
-          name="passVerify"
-          value={passVerify}
-          onChange={handlePassVerify}
-        />
-
-        <button type="submit">Sign Up</button>
+        <button
+          className="bg-orange-400 border-2 shadow border-orange-500 rounded w-full py-2.5 hover:bg-orange-600 hover:border-orange-700 hover:border-2"
+          type="submit"
+        >
+          Sign Up
+        </button>
       </form>
 
       {errorMessage && <p className="error-message">{errorMessage}</p>}
 
-      <p>Already have account?</p>
-      <Link to={"/login"}> Login</Link>
+      <div className="flex gap-2">
+        <p>Already have account?</p>
+        <Link className="text-orange-700" to={"/login"}> Login</Link>
+      </div>
     </div>
   );
 }
-
-export default SignupPage;
