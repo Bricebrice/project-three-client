fc635e47b05199341e80785bf62644982d435e5e
Ricardo Vazquez <rickyrabv@gmail.com>
2024-03-16 11:25:04 +0100

    meal service debug

--- src/services/meal.service.js
+++ src/services/meal.service.js
@@ -3,7 +3,7 @@ import axios from "axios";
 class MealService {
   constructor() {
     this.api = axios.create({
-      baseURL: import.meta.env.REACT_APP_API_URL,
+      baseURL: import.meta.env.VITE_DEPLOYMENT_URL,
     });
   }
 
@@ -19,7 +19,7 @@ class MealService {
     return this.api.post("/meal/create-new-meal", requestBody);
   };
 
-  
+
 }
 
 // Create one instance (object) of the service
