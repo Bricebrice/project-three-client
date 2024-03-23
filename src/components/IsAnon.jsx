9ff3460770b3983ccaab77d25afe6501d9e3b40e
Brice Cabioch <cabioch.brice@gmail.com>
2024-03-23 14:17:31 +0100

    import Spinner missing from IsAnon and IsPrivate

--- src/components/IsAnon.jsx
+++ src/components/IsAnon.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/auth.context";
 import { Navigate } from "react-router-dom";
+import VegSpinner from "./Spinner";
 
 function IsAnon({ children }) {
   const { isLoggedIn, isLoading } = useContext(AuthContext);
