5cb58934a56243d155d8c2a83b7cc95d3f0b0595
Ricardo Vazquez <rickyrabv@gmail.com>
2024-03-26 21:09:12 +0100

    styling profilepage and no double ingredients secured

--- src/pages/Auth/ProfilePage.jsx
+++ src/pages/Auth/ProfilePage.jsx
@@ -6,6 +6,7 @@ import userService from "../../services/user.service";
 import VegSpinner from "../../components/VegSpinner";
 import Footer from "../../components/Footer";
 import { AuthContext } from "../../context/auth.context";
+import logo from "../../assets/logo.png"
 
 export default function ProfilePage() {
   const { user } = useContext(AuthContext);
@@ -33,10 +34,31 @@ export default function ProfilePage() {
   }
 
   return (
-    <div className="bg-yellow-100 h-dvh">
-      <ProfileHeader />
-      <FavoriteMeals favMeals={favMeals} />
-      <FavoriteIngredients favIngredients={favIngredients} url="/ingredient" />
+    <div className="">
+      <div className="bg-yellow-100 py-10">
+        <ProfileHeader />
+        <div className="flex flex-col lg:flex-row gap-5 lg:gap-1 w-full items-center lg:justify-center lg:items-baseline pb-24">
+          <div className="bg-mantis-100 lg:w-1/2 rounded-md mx-2 py-3 w-11/12 lg:max-w-lg">
+            <FavoriteMeals favMeals={favMeals} />
+            <FavoriteIngredients
+              favIngredients={favIngredients}
+              url="/ingredient"
+            />
+          </div>
+          <div className="bg-mantis-100 lg:w-1/2 w-11/12 rounded-md flex flex-col items-center text-center lg:max-w-lg">
+            <h1 className="text-3xl mx-3 my-2">Coming soon!</h1>
+            <p className="text-lg mx-3 mb-5">
+              We ask for your patience as we are still develping our calendar
+              functionality. Stay tuned and in touch as we continue to grow our
+              project!
+            </p>
+            <div className="bg-white rounded-md w-48 h-48 flex items-center justify-center mb-5">
+              <img src={logo} alt="" className="w-32 h-32"/>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <Footer />
     </div>
   );
